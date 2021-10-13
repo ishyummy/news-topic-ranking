d04da559695d2a1a1dc55e701616aafb5b946fd2
ishyummy <asiapjh@naver.com>
2021-10-13 23:52:38 +0900

    210615

---
+++ Project2/Project2.cpp
@@ -0,0 +1,482 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+#include <time.h>
+#include <stdlib.h>
+#include <wininet.h> 
+#pragma comment (lib, "wininet.lib")
+
+char titles[300][100];
+char address[100] = "board/lists/?id=hiphop&page=1&exception_mode=recommend";
+char stopwords[1000][20];
+int idx = -1, stop_idx, key_idx = -1;
+
+typedef struct keyword {
+	char word[100];
+	int count = 0;
+} keyword;
+keyword keywords[1000];
+
+void set_stop() {
+	int i = -1;
+	strcpy(stopwords[++i], "아하");
+	strcpy(stopwords[++i], "오호");
+	strcpy(stopwords[++i], "어허");
+	strcpy(stopwords[++i], "하나");
+	strcpy(stopwords[++i], "둘");
+	strcpy(stopwords[++i], "셋");
+	strcpy(stopwords[++i], "넷");
+	strcpy(stopwords[++i], "다섯");
+	strcpy(stopwords[++i], "여섯");
+	strcpy(stopwords[++i], "일곱");
+	strcpy(stopwords[++i], "여덟");
+	strcpy(stopwords[++i], "아홉");
+	strcpy(stopwords[++i], "열");
+	strcpy(stopwords[++i], "열하나");
+	strcpy(stopwords[++i], "열둘");
+	strcpy(stopwords[++i], "열셋");
+	strcpy(stopwords[++i], "열넷");
+	strcpy(stopwords[++i], "열다섯");
+	strcpy(stopwords[++i], "열여섯");
+	strcpy(stopwords[++i], "열일곱");
+	strcpy(stopwords[++i], "열여덟");
+	strcpy(stopwords[++i], "열아홉");
+	strcpy(stopwords[++i], "스물");
+	strcpy(stopwords[++i], "우리");
+	strcpy(stopwords[++i], "저희");
+	strcpy(stopwords[++i], "따라");
+	strcpy(stopwords[++i], "의해");
+	strcpy(stopwords[++i], "에게");
+	strcpy(stopwords[++i], "래퍼");
+	strcpy(stopwords[++i], "의거");
+	strcpy(stopwords[++i], "의거하여");
+	strcpy(stopwords[++i], "때문에");
+	strcpy(stopwords[++i], "때문");
+	strcpy(stopwords[++i], "하지마");
+	strcpy(stopwords[++i], "해줘");
+	strcpy(stopwords[++i], "먹었다");
+	strcpy(stopwords[++i], "받았다");
+	strcpy(stopwords[++i], "이쯤에서");
+	strcpy(stopwords[++i], "취향인");
+	strcpy(stopwords[++i], "취향");
+	strcpy(stopwords[++i], "빠는");
+	strcpy(stopwords[++i], "애들");
+	strcpy(stopwords[++i], "프로");
+	strcpy(stopwords[++i], "확률로");
+	strcpy(stopwords[++i], "있음");
+	strcpy(stopwords[++i], "없음");
+	strcpy(stopwords[++i], "있냐");
+	strcpy(stopwords[++i], "없냐");
+	strcpy(stopwords[++i], "보자");
+	strcpy(stopwords[++i], "저격");
+	strcpy(stopwords[++i], "사실");
+	strcpy(stopwords[++i], "아니");
+	strcpy(stopwords[++i], "거임");
+	strcpy(stopwords[++i], "거야");
+	strcpy(stopwords[++i], "유난히");
+	strcpy(stopwords[++i], "생각");
+	strcpy(stopwords[++i], "해봤는데");
+	strcpy(stopwords[++i], "들어봤는데");
+	strcpy(stopwords[++i], "별로");
+	strcpy(stopwords[++i], "예고");
+	strcpy(stopwords[++i], "해라");
+	strcpy(stopwords[++i], "이유");
+	strcpy(stopwords[++i], "인스타");
+	strcpy(stopwords[++i], "스토리");
+	strcpy(stopwords[++i], "라방");
+	strcpy(stopwords[++i], "페북");
+	strcpy(stopwords[++i], "이건");
+	strcpy(stopwords[++i], "진짜");
+	strcpy(stopwords[++i], "가능");
+	strcpy(stopwords[++i], "신곡");
+	strcpy(stopwords[++i], "오늘");
+	strcpy(stopwords[++i], "내일");
+	strcpy(stopwords[++i], "모레");
+	strcpy(stopwords[++i], "sns");
+	strcpy(stopwords[++i], "jpg");
+	strcpy(stopwords[++i], "jp");
+	strcpy(stopwords[++i], "gif");
+	strcpy(stopwords[++i], "avi");
+	strcpy(stopwords[++i], "av");
+	strcpy(stopwords[++i], "mp4");
+	strcpy(stopwords[++i], "mp");
+	strcpy(stopwords[++i], "txt");
+	strcpy(stopwords[++i], "tx");
+	strcpy(stopwords[++i], "feat");
+	strcpy(stopwords[++i], "remix");
+	strcpy(stopwords[++i], "그리고");
+	strcpy(stopwords[++i], "그러나");
+	strcpy(stopwords[++i], "비록");
+	strcpy(stopwords[++i], "더라도");
+	strcpy(stopwords[++i], "아니면");
+	strcpy(stopwords[++i], "제외하고");
+	strcpy(stopwords[++i], "비로소");
+	strcpy(stopwords[++i], "부터");
+	strcpy(stopwords[++i], "까지");
+	strcpy(stopwords[++i], "이랑");
+	strcpy(stopwords[++i], "엄마");
+	strcpy(stopwords[++i], "아빠");
+	strcpy(stopwords[++i], "애미");
+	strcpy(stopwords[++i], "애비");
+	strcpy(stopwords[++i], "대해서");
+	strcpy(stopwords[++i], "남짓");
+	strcpy(stopwords[++i], "약간");
+	strcpy(stopwords[++i], "다소");
+	strcpy(stopwords[++i], "대박");
+	strcpy(stopwords[++i], "어느정도");
+	strcpy(stopwords[++i], "대부분");
+	strcpy(stopwords[++i], "거의");
+	strcpy(stopwords[++i], "당장");
+	strcpy(stopwords[++i], "결국");
+	strcpy(stopwords[++i], "차라리");
+	strcpy(stopwords[++i], "제발");
+	strcpy(stopwords[++i], "애초에");
+	strcpy(stopwords[++i], "해요");
+	strcpy(stopwords[++i], "다음");
+	strcpy(stopwords[++i], "어때");
+	strcpy(stopwords[++i], "녹음");
+	strcpy(stopwords[++i], "해주세요");
+	strcpy(stopwords[++i], "해줘");
+	strcpy(stopwords[++i], "할까");
+	strcpy(stopwords[++i], "하고싶다");
+	strcpy(stopwords[++i], "사람");
+	strcpy(stopwords[++i], "이기");
+	strcpy(stopwords[++i], "이새끼");
+	strcpy(stopwords[++i], "이랑");
+	strcpy(stopwords[++i], "여서");
+	strcpy(stopwords[++i], "냠");
+	strcpy(stopwords[++i], "힙붕이");
+	strcpy(stopwords[++i], "나눈");
+	strcpy(stopwords[++i], "존나");
+	strcpy(stopwords[++i], "노래");
+	strcpy(stopwords[++i], "힙붕");
+	strcpy(stopwords[++i], "힙합");
+	strcpy(stopwords[++i], "힙갤");
+	strcpy(stopwords[++i], "국힙");
+	strcpy(stopwords[++i], "와꾸");
+	strcpy(stopwords[++i], "솔직히");
+	strcpy(stopwords[++i], "민심");
+	strcpy(stopwords[++i], "테스트");
+	strcpy(stopwords[++i], "솔직하게");
+	strcpy(stopwords[++i], "소신발언");
+	strcpy(stopwords[++i], "잼민이");
+	strcpy(stopwords[++i], "잼민이들");
+	strcpy(stopwords[++i], "대해");
+	strcpy(stopwords[++i], "충격");
+	strcpy(stopwords[++i], "다이어트");
+	strcpy(stopwords[++i], "비교");
+	strcpy(stopwords[++i], "확실히");
+	strcpy(stopwords[++i], "확실하게");
+	strcpy(stopwords[++i], "하네");
+	strcpy(stopwords[++i], "힙알못");
+	strcpy(stopwords[++i], "힙잘알");
+	strcpy(stopwords[++i], "후기");
+	strcpy(stopwords[++i], "속보");
+	strcpy(stopwords[++i], "내가");
+	strcpy(stopwords[++i], "친분");
+	strcpy(stopwords[++i], "인맥");
+	strcpy(stopwords[++i], "근황");
+	strcpy(stopwords[++i], "실시간");
+	strcpy(stopwords[++i], "오늘자");
+	strcpy(stopwords[++i], "ㄹㅇ");
+	strcpy(stopwords[++i], "이거");
+	strcpy(stopwords[++i], "저거");
+	strcpy(stopwords[++i], "이게");
+	strcpy(stopwords[++i], "저게");
+	strcpy(stopwords[++i], "까이");
+	strcpy(stopwords[++i], "뭐냐");
+	strcpy(stopwords[++i], "예고");
+	strcpy(stopwords[++i], "빠르고");
+	strcpy(stopwords[++i], "빡센");
+	strcpy(stopwords[++i], "아깝긴");
+	strcpy(stopwords[++i], "소름인");
+	strcpy(stopwords[++i], "장문");
+	strcpy(stopwords[++i], "논란");
+	strcpy(stopwords[++i], "한테");
+	strcpy(stopwords[++i], "대체");
+	strcpy(stopwords[++i], "좀");
+	strcpy(stopwords[++i], "등");
+	strcpy(stopwords[++i], "해");
+	strcpy(stopwords[++i], "살");
+	strcpy(stopwords[++i], "휴");
+	strcpy(stopwords[++i], "뿐");
+	strcpy(stopwords[++i], "쫌");
+	strcpy(stopwords[++i], "즉");
+	strcpy(stopwords[++i], "퍽");
+	strcpy(stopwords[++i], "팍");
+	strcpy(stopwords[++i], "헉");
+	strcpy(stopwords[++i], "퉤");
+	strcpy(stopwords[++i], "쳇");
+	strcpy(stopwords[++i], "점");
+	strcpy(stopwords[++i], "또");
+	strcpy(stopwords[++i], "곧");
+	stop_idx = i;
+}
+
+void set_clear() { // 전처리 과정, 소문자 대문자로 바꾸고 숫자 없애자
+	for (int i = 0; i <= idx; i++) {
+		for (int j = 0; j < strlen(titles[i]); j++) {
+			if ('A' <= titles[i][j] && titles[i][j] <= 'Z')
+				titles[i][j] += 'a' - 'A';
+			else if ('0' <= titles[i][j] && titles[i][j] <= '9')
+				titles[i][j] = ' ';
+			else if (titles[i][j] == '-' || titles[i][j] == '★' || titles[i][j] == '=' || titles[i][j] == '#')
+				titles[i][j] = ' ';
+			else if (titles[i][j] == '^' || titles[i][j] == '*' || titles[i][j] == '?' || titles[i][j] == '!' || titles[i][j] == '~' || titles[i][j] == '.')
+				titles[i][j] = ' ';
+			else if (titles[i][j] == ')' || titles[i][j] == '(')
+				titles[i][j] = ' ';
+			else {
+				char temp[10];
+				temp[0] = titles[i][j];
+				temp[1] = titles[i][j + 1];
+				temp[2] = '\0';
+
+				if (!strcmp(temp, "가") || !strcmp(temp, "이") || !strcmp(temp, "는") || !strcmp(temp, "은") || !strcmp(temp, "를") || !strcmp(temp, "을") || !strcmp(temp, "도") || !strcmp(temp, "와") || !strcmp(temp, "과")|| !strcmp(temp, "다")) {
+					if(j < strlen(titles[i]) - 2)
+						if (titles[i][j + 2] == ' ') {
+							titles[i][j] = ' ';
+							titles[i][j + 1] = ' ';
+						}
+				}
+				else if (!strcmp(temp, "ㄱ") || !strcmp(temp, "ㄴ") || !strcmp(temp, "ㄷ") || !strcmp(temp, "ㅋ") || !strcmp(temp, "ㄹ") || !strcmp(temp, "ㅇ") || !strcmp(temp, "ㅈ") || !strcmp(temp, "ㅉ")) {
+					titles[i][j] = ' ';
+					titles[i][j + 1] = ' ';
+				}
+			}
+		}
+	}
+}
+
+void set_keyword() {
+	char temp[100];
+	char* cpo;
+
+	for (int i = 0; i <= idx; i++) {
+		for (int j = 0; j <= stop_idx; j++) { // 먼저 불용어들 모두 space로 치환
+			cpo = strstr(titles[i], stopwords[j]);
+			if (cpo) {
+				for (int k = 0; k < strlen(stopwords[j]); k++)
+					cpo[k] = ' ';
+			}
+		}
+
+		int flag = 0, t = -1;
+		for (int j = 0; j < strlen(titles[i]); j++) { // 남는 단어들 모두 keyword에 추가
+			if (titles[i][j] == '<')
+				break;
+			if (titles[i][j] == ' ' || titles[i][j] == '\0') {
+				if (flag) {
+					temp[++t] = '\0';
+					flag = 0;
+					t = -1;
+
+					int find = -1;
+					for (int k = 0; k <= key_idx; k++)
+						if (!strcmp(keywords[k].word, temp)) {
+							find = k;
+							break;
+						}
+					if (find == -1) { // 새로운 키워드 추가
+						strcpy(keywords[++key_idx].word, temp);
+						keywords[key_idx].count = 1;
+					}
+					else
+						keywords[find].count++;
+				}
+			}
+			else {
+				flag = 1;
+				temp[++t] = titles[i][j];
+			}
+		}
+	}
+}
+
+void sort_keyword() {
+	int a, b, c, d;
+
+	for (int i = 0; i <= key_idx; i++) { // 쓰레기 단어들 다 -1 할당
+		int len = strlen(keywords[i].word);
+		if (len <= 3) {
+			keywords[i].count = -1;
+		}
+		if (!strcmp(keywords[i].word, "힙뀽")) {
+			a = i;
+		}
+		if (!strcmp(keywords[i].word, "힙뀽이")) {
+			b = i;
+		}
+		if (!strcmp(keywords[i].word, "젓딧")) {
+			c = i;
+		}
+		if (!strcmp(keywords[i].word, "저스디스")) {
+			d = i;
+		}
+	}
+	keywords[b].count += keywords[a].count;
+	keywords[a].count = -1;
+	keywords[d].count += keywords[c].count;
+	keywords[c].count = -1;
+
+	for(int i = 0; i <= key_idx; i++)
+		for (int j = 0; j <= key_idx - i - 1; j++) {
+			if (keywords[j].count < keywords[j + 1].count) {
+				int temp = keywords[j].count;
+				keywords[j].count = keywords[j + 1].count;
+				keywords[j + 1].count = temp;
+
+				char tem[100];
+				strcpy(tem, keywords[j].word);
+				strcpy(keywords[j].word, keywords[j + 1].word);
+				strcpy(keywords[j + 1].word, tem);
+			}
+		}
+
+}
+
+DWORD ReadHtmlText(HINTERNET ah_http_file, char *ap_html_string)
+{
+	char buffer[1025];
+	DWORD read_byte = 0, error_count = 0, total_bytes = 0;
+	while (InternetReadFile(ah_http_file, buffer, 1024, &read_byte)) {
+		memcpy(ap_html_string + total_bytes, buffer, read_byte);
+		total_bytes += read_byte;
+		if (read_byte < 1024) {
+			error_count++;
+			if (error_count > 10) break;
+			else Sleep(5); // 5ms 지연
+		}
+		else error_count = 0;
+	}
+
+	*(ap_html_string + total_bytes) = 0;
+	return total_bytes;
+}
+
+void LoadDataFromWebPage()
+{
+	HINTERNET h_session = InternetOpen("Hiphop", PRE_CONFIG_INTERNET_ACCESS, NULL, INTERNET_INVALID_PORT_NUMBER, 0);
+	HINTERNET h_connect = InternetConnect(h_session, "gall.dcinside.com", INTERNET_INVALID_PORT_NUMBER, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+	HINTERNET h_http_file = HttpOpenRequest(h_connect, "GET", address, HTTP_VERSION, NULL, 0, INTERNET_FLAG_DONT_CACHE, 0);
+
+	char *p_utf8_html_str = (char *)malloc(1024 * 1024);
+	if (p_utf8_html_str != NULL) {
+		if (HttpSendRequest(h_http_file, NULL, 0, 0, 0) == TRUE) {
+
+			ReadHtmlText(h_http_file, p_utf8_html_str);
+
+			char *p_ascii_html_str = NULL;
+
+			Utf8ToAscii(&p_ascii_html_str, p_utf8_html_str);
+			if (p_ascii_html_str != NULL) {
+				SetCtrlName(FindControl(1000), p_ascii_html_str);
+				free(p_ascii_html_str);
+			}
+		}
+		free(p_utf8_html_str);
+	}
+
+	if (h_http_file != NULL) InternetCloseHandle(h_http_file);
+	if (h_connect != NULL) InternetCloseHandle(h_connect);
+	if (h_session != NULL) InternetCloseHandle(h_session);
+}
+
+char *CopyTextFromWebData(char *ap_dest_str, char *ap_src_str, const char *ap_start_word, const char *ap_end_word)
+{
+	char *p_pos = strstr(ap_src_str, ap_start_word);
+	if (p_pos != NULL) {
+		char *p_limit_pos = strstr(p_pos, ap_end_word);
+		if (p_limit_pos != NULL) {
+			p_pos += strlen(ap_start_word);
+			while (p_pos < p_limit_pos) {
+				*ap_dest_str++ = *p_pos++;
+			}
+			*ap_dest_str = 0;
+			return p_pos;
+		}
+	}
+	return ap_src_str; 
+}
+
+
+void ShowRank()
+{
+	for (int i = 0; i < 3; i++) { // 3 page 까지 읽기
+		switch (i) {
+		case 0 :
+			address[28] = '1';
+			break;
+		case 1:
+			address[28] = '2';
+			break;
+		case 2:
+			address[28] = '3';
+			break;
+		}
+
+		LoadDataFromWebPage(); // html 가져오기
+
+		void *p_edit = FindControl(1000);
+		int len = Edit_GetLength(p_edit) + 1;
+		char time_str[1000];
+		char *p_html_str = (char *)malloc(len);
+		if (p_html_str != NULL) {
+			GetCtrlName(p_edit, p_html_str, len);
+
+			char *p_pos;
+			do {
+				p_pos = strstr(p_html_str, "view-msg =\"\">");
+				if (p_pos != NULL) {
+					p_pos[0] = 'x';
+					p_pos += 12;
+
+					p_pos = CopyTextFromWebData(time_str, p_pos, "</em>", "</a>");
+					strcpy(titles[++idx], time_str);
+				}
+			} while (p_pos != NULL); // 글 제목만 titles에 저장
+
+			free(p_html_str);
+		}
+	}
+
+	set_clear(); // 전처리 과정
+	set_keyword(); // stopwords 제외하고 키워드만 추출
+	sort_keyword(); // 의미없는 키워드 제외, 정렬
+	int asd = 0;
+	
+	for (int j = 1; j <= 20; j++) {
+		SelectFontObject("Consolas", 18, 1);
+		TextOut(100, 115 + (++asd) * 20, RGB(200, 240, 250), "%2d : ", j);
+		SelectFontObject("나눔고딕", 18, 1);
+		TextOut(210, 115 + (asd) * 20, RGB(240, 255, 160), "%8s", keywords[j].word);
+		SelectFontObject("나눔고딕", 12, 1);
+		TextOut(430, 115 + (asd) * 20, RGB(150, 180, 160), "%2d회", keywords[j].count);
+	}
+		
+	ShowDisplay();
+}
+
+void OnCommand(INT32 a_ctrl_id, INT32 a_notify_code, void *ap_ctrl)
+{
+	//if (a_ctrl_id == 1011) ShowRank();
+}
+
+CMD_MESSAGE(OnCommand)
+
+int main()
+{
+	ChangeWorkSize(500, 600);
+	Clear(0, RGB(82, 97, 124));
+
+
+	CreateEdit(5, 5, 490, 100, 1000, ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | ES_WANTRETURN | WS_VSCROLL | WS_HSCROLL);
+
+	CreateButton("랭킹 새로고침", 150, 560, 200, 28, 1011);
+
+	ShowDisplay();
+
+	set_stop();
+	ShowRank();
+
+	return 0;
+}
---
+++ Project2/movie.cpp
@@ -0,0 +1,326 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+
+// HTTP 프로토콜을 사용하는 함수들이 정의된 wininet.h 헤더 파일을 추가한다.
+#include <wininet.h> 
+// wininet.h 헤더에 정의된 함수들을 사용하려면 wininet.lib 라이브러리를 추가해야 한다.
+#pragma comment (lib, "wininet.lib")
+
+struct MovieData
+{
+	char ico_rate[16];             // 관람 등급
+	char title[64];                // 제목
+	char want[12];                 // 좋아요
+	char dont_want[12];            // 글쎄요
+	char genre[128];               // 장르
+	char etc[32];                  // 상영 정보
+	char director[128];            // 감독
+	char actor[256];               // 연기자
+	unsigned short ico_rate_len;   // 관람 등급 문자열 길이
+	unsigned short title_len;      // 제목 문자열 길이
+	unsigned short want_len;       // 좋아요 문자열 길이
+	unsigned short dont_want_len;  // 글쎄요 문자열 길이
+	unsigned short genre_len;      // 장르 문자열 길이
+	unsigned short director_len;   // 감독 문자열 길이
+	unsigned short actor_len;      // 연기자 문자열 길이
+	unsigned short etc_len;        // 상영 정보 문자열 길이
+};
+
+DWORD ReadHtmlText(HINTERNET ah_http_file, char *ap_html_string)
+{
+	// 잠시 지연 효과를 메인 스레드에게만 사용하기 위해 이벤트 객체를 생성한다.
+	HANDLE h_wait_event = CreateEvent(NULL, TRUE, 0, NULL);
+	if (h_wait_event == NULL) return 0;  // 이벤트 객체 생성에 실패한 경우
+
+	char buffer[1025];
+	DWORD read_byte = 0, error_count = 0, total_bytes = 0;
+	// 1024 바이트 단위로 HTML 코드를 가져옴
+	while (InternetReadFile(ah_http_file, buffer, 1024, &read_byte)) {
+		// 1024 단위로 가져온 HTML 소스를 ap_html_string에 계속 추가한다.
+		memcpy(ap_html_string + total_bytes, buffer, read_byte);
+		total_bytes += read_byte;  // 읽은 전체 바이트 수를 갱신한다.
+		if (read_byte < 1024) {
+			// 1024바이트씩 요청하더라도 해당 페이지의 HTML 소스가 1024보다 작거나
+			// 네트워크 지연으로 더 작은 크기가 전송될수 있음. (10번 정도 재시도함)
+			error_count++;
+			if (error_count > 10) break;
+			else WaitForSingleObject(h_wait_event, 5); // 5ms 지연하도록 구성한다.
+		}
+		else error_count = 0;
+	}
+
+	*(ap_html_string + total_bytes) = 0;  // 문자열의 끝에 NULL 문자를 추가함!
+	CloseHandle(h_wait_event);  // 지연 효과를 위해 생성한 이벤트 객체를 제거한다.
+	return total_bytes;  // 읽어들인 전체 바이트 수를 반환한다.
+}
+
+// 분석에 의미없는 문자들을 제거하는 함수!
+void RemoveMeaninglessChar(char *ap_string)
+{
+	// 탭, 줄바꿈 문자가 나올때까지 반복한다.
+	while (*ap_string) {
+		if (*ap_string == '\t' || *ap_string == '\r' || *ap_string == '\n') break;
+		ap_string++;
+	}
+	if (!*ap_string) return;  // 전체적으로 탭, 줄바꿈 문자가 없는 경우!
+
+	char *p_dest = ap_string++;
+	while (*ap_string) {
+		// 탭, 줄바꿈 문자가 아닌 경우에만 복사를 진행한다.
+		if (*ap_string != '\t' && *ap_string != '\r' && *ap_string != '\n') {
+			*p_dest++ = *ap_string;
+		}
+		ap_string++;
+	}
+	*p_dest = 0;  // 문자열의 끝에 NULL 문자를 추가한다.
+}
+
+void LoadDataFromWebPage()  // 웹 페이지를 구성하는 HTML 소스를 가져오는 함수
+{
+	// 인터넷을 사용할 세션을 구성한다.
+	HINTERNET h_session = InternetOpen("MovieScanner", PRE_CONFIG_INTERNET_ACCESS, NULL, INTERNET_INVALID_PORT_NUMBER, 0);
+	// 어떤 사이트에 접속할지 구성한다. (네이버 영화를 사용하도록 구성한다.)
+	HINTERNET h_connect = InternetConnect(h_session, "movie.naver.com", INTERNET_INVALID_PORT_NUMBER, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+	// 네이버 영화에서 개봉 예정영화 페이지에 접속한다. (url 배열에 저장된 페이지를 연다.)
+	HINTERNET h_http_file = HttpOpenRequest(h_connect, "GET", "/movie/running/premovie.nhn", HTTP_VERSION, NULL, 0, INTERNET_FLAG_DONT_CACHE, 0);
+
+	char *p_utf8_html_str = (char *)malloc(2 * 1024 * 1024);  // 2M Bytes 메모리 할당!
+	if (p_utf8_html_str != NULL) {  // 메모리 할당에 성공했다면!
+		if (HttpSendRequest(h_http_file, NULL, 0, 0, 0) == TRUE) { // 웹 페이지 소스를 요청한다.
+			// 웹 페이지 전체 소스를 할당된 메모리에 복사한다.
+			ReadHtmlText(h_http_file, p_utf8_html_str);
+			// HTML 소스 분석에 영향을 미치지 않는 탭, 줄바꿈 문자를 제거한다.
+			RemoveMeaninglessChar(p_utf8_html_str);
+
+			char *p_ascii_html_str = NULL;
+			// 가져온 웹 페이지 소스가 UTF8 형식의 문자열이라서 ASCII 형식의 문자열로 변환합니다.
+			Utf8ToAscii(&p_ascii_html_str, p_utf8_html_str);
+			if (p_ascii_html_str != NULL) {  // 변환에 성공했다면
+				// 변환된 HTML 소스를 1000번 에디트 컨트롤에 표시한다.
+				SetCtrlName(FindControl(1000), p_ascii_html_str);
+				free(p_ascii_html_str);  // 변환에 사용했던 메모리를 해제한다.
+			}
+		}
+		free(p_utf8_html_str);  // 웹 페이지 소스를 저장하기 위해 할당했던 메모리를 해제한다.
+	}
+
+	// 웹 페이지를 사용하기 위해 할당했던 핸들을 모두 해제한다.
+	if (h_http_file != NULL) InternetCloseHandle(h_http_file);
+	if (h_connect != NULL) InternetCloseHandle(h_connect);
+	if (h_session != NULL) InternetCloseHandle(h_session);
+}
+
+void DestoryMovieData()
+{
+	void *p_data, *p_ctrl = FindControl(1001);  // 사용자 목록이 저장된 리스트 박스의 주소를 얻는다.
+	int count = ListBox_GetCount(p_ctrl); // 리스트 박스에 추가된 항목의 수를 얻는다.  
+	for (int i = 0; i < count; i++) {  // 리스트 박스의 각 항목에 추가된 메모리를 모두 제거한다.
+		p_data = ListBox_GetItemDataPtr(p_ctrl, i);  // i 번째 항목에 저장된 주소를 가져온다.
+		free(p_data); // p_data가 가리키는 메모리를 해제한다.
+	}
+	ListBox_ResetContent(p_ctrl);  // 리스트 박스의 모든 항목을 제거한다.
+}
+
+// "> 문자열 뒤부터 < 문자가 나올때까지 문자열을 복사하는 함수! 복사한 문자열의 길이도 알려줌!
+char *CopyStringFromWebData(char *ap_dest_str, unsigned short *ap_dest_len, char *ap_src_str)
+{
+	ap_src_str = strstr(ap_src_str, "\">");  // "> 문자열 위치를 찾는다.
+	if (ap_src_str != NULL) {  // 찾았다면!
+		char *p_start_pos = ap_dest_str;  // 탐색전에 복사할 메모리의 시작 위치를 기억한다.
+		ap_src_str += 2;  // "> 문자열은 제외한다.
+		// < 문자가 나올때까지 문자열을 복사한다.
+		while (*ap_src_str && *ap_src_str != '<') *ap_dest_str++ = *ap_src_str++;
+		*ap_dest_str = 0;  // NULL 문자를 추가한다.
+		*ap_dest_len = ap_dest_str - p_start_pos;  // 문자열의 길이를 계산해서 대입한다.
+	}
+	return ap_src_str;  // 작업에 실패했으면 전달받은 위치를 그대로 반환한다.
+}
+
+// 영화 페이지에 있는 LinkText 항목을 기준으로 문자열을 복사할 때 사용하는 함수!
+char *CopyLinkTextFromWebData(char *ap_dest_str, unsigned short *ap_dest_len, char *ap_src_str)
+{
+	char *p_limit_pos = strstr(ap_src_str, "</span>");   // </span> 문자열을 사용해서 마지막 위치를 찾는다.
+	if (p_limit_pos != NULL) {  // 찾았다면!
+		char *p_start_pos = ap_dest_str, open_flag = 0;
+		//  <  > 로 포함된 문자열은 제외하고 나머지 문자열만 복사한다.
+		while (ap_src_str < p_limit_pos) {
+			if (*ap_src_str == '<') open_flag = 1;
+			else if (*ap_src_str == '>') open_flag = 0;
+			else {
+				if (!open_flag) *ap_dest_str++ = *ap_src_str;
+			}
+			ap_src_str++;
+		}
+		*ap_dest_str = 0;  // NULL 문자를 추가한다.
+		*ap_dest_len = ap_dest_str - p_start_pos;  // 문자열의 길이를 계산해서 대입한다.
+		return p_limit_pos + 7;  // +7은 </span> 길이만큼 지나감!
+	}
+	return ap_src_str; // 작업에 실패했으면 전달받은 위치를 그대로 반환한다.
+}
+
+// 상영 시간과 개봉 일자를 읽을 때 사용하는 함수!
+char *CopyEtcDataFromWebData(char *ap_dest_str, unsigned short *ap_dest_len, char *ap_src_str)
+{
+	char *p_limit_pos = strstr(ap_src_str, "</dd>");  // </dd> 문자열을 사용해서 마지막 위치를 찾는다.
+	if (p_limit_pos != NULL) {  // 찾았다면!
+		char *p_start_pos = ap_dest_str, open_flag = 0;
+		//  <  > 로 포함된 문자열은 제외하고 나머지 문자열만 복사한다.
+		while (ap_src_str < p_limit_pos) {
+			if (*ap_src_str == '<') open_flag = 1;
+			else if (*ap_src_str == '>') open_flag = 0;
+			else {
+				if (!open_flag) {
+					// 중간에 '|' 문자를 만나면 ", " 문자열로 변경해서 추가한다.
+					if (*ap_src_str == '|') {
+						if (p_start_pos != ap_dest_str) {
+							*ap_dest_str++ = ',';
+							*ap_dest_str++ = ' ';
+						}
+					}
+					else *ap_dest_str++ = *ap_src_str;
+				}
+			}
+			ap_src_str++;
+		}
+		*ap_dest_str = 0;  // NULL 문자를 추가한다.
+		*ap_dest_len = ap_dest_str - p_start_pos; // 문자열의 길이를 계산해서 대입한다.
+		return p_limit_pos + 5;  // +5는 </dd> 길이만큼 지나감!
+	}
+	return ap_src_str;
+}
+
+
+char *AddMovieInfo(char *ap_string)
+{
+	// 상영작 정보를 저장하기 위한 메모리를 할당한다!
+	MovieData *p_movie_data = (MovieData *)malloc(sizeof(MovieData));
+	if (p_movie_data == NULL) return NULL;  // 메모리 시스템 오류 발생!!
+
+	memset(p_movie_data, 0, sizeof(MovieData));  // 할당된 메모리를 초기화한다.
+	void *p_movie_list = FindControl(1001);   // 영화 목록을 저장할 리스트 박스의 주소를 얻는다.
+	char *p_pos = strstr(ap_string, "<span class=\"");  // 각 항목 정보들은 <span class=" 로 구분된다!
+
+	while (p_pos != NULL) {
+		p_pos += 13;  // + 13는 <span class=" 길이 만큼 지나감!
+
+		if (!strncmp(p_pos, "ico_rating_", 11)) {  // 관람 기준 정보인 경우!
+			p_pos = CopyStringFromWebData(p_movie_data->ico_rate, &p_movie_data->ico_rate_len, p_pos + 11);  // + 11는 "ico_rating 길이 만큼 지나감
+			p_pos = CopyStringFromWebData(p_movie_data->title, &p_movie_data->title_len, p_pos + 7);  // +7은 </span> 길이 만큼 지나감
+		}
+		else if (!strncmp(p_pos, "num", 3)) {  // 예매율!
+		 // 현재 사용 안함! 나중에 필요하면 구현하세요!
+			p_pos = strstr(p_pos, "</dd>"); // </dd> 까지 지나감!
+		}
+		else if (!strncmp(p_pos, "ico_want", 8)) {  // 기대지수
+			p_pos = strstr(p_pos + 8, "\"exp_cnt\"");  // +8은 ico_want 길이 만큼 지나감
+			if (p_pos == NULL) break;
+			p_pos = CopyStringFromWebData(p_movie_data->want, &p_movie_data->want_len, p_pos + 8);  // +8은 "exp_cnt 길이 만큼 지나감
+			p_pos = strstr(p_pos, "\"exp_cnt\"");
+			p_pos = CopyStringFromWebData(p_movie_data->dont_want, &p_movie_data->dont_want_len, p_pos + 8);  // +8은 "exp_cnt 길이 만큼 지나감
+			p_pos += 25;  //  기대 지수 뒤에 있는 문자열 지나감!
+		}
+		else if (!strncmp(p_pos, "link_txt", 8)) {  // 영화 장르, 감독 또는 연기자!
+			p_pos += 10;  // +10은 "link_text"> 길이 만큼 지나감
+			if (!strncmp(p_pos - 36, "개요", 4)) {
+				p_pos = CopyLinkTextFromWebData(p_movie_data->genre, &p_movie_data->genre_len, p_pos);  // 장르 정보!
+				p_pos = CopyEtcDataFromWebData(p_movie_data->etc, &p_movie_data->etc_len, p_pos);  // 상영 시간과 개봉일자!
+			}
+			else if (!strncmp(p_pos - 36, "감독", 4)) {
+				p_pos = CopyLinkTextFromWebData(p_movie_data->director, &p_movie_data->director_len, p_pos);  // 감독 정보!
+			}
+			else {  // 출연진!
+				p_pos = CopyLinkTextFromWebData(p_movie_data->actor, &p_movie_data->actor_len, p_pos);  // 출연진 정보!!
+			}
+		}
+		else if (!strncmp(p_pos, "btn_", 4)) {
+			// p_movie_data에 구성된 영화 정보를 리스트 박스에 추가한다.
+			ListBox_SetItemDataPtrEx(p_movie_list, -1, "", p_movie_data, 0);
+			break;  // 영화 한 편에 대한 정보가 끝났음
+		}
+		p_pos = strstr(p_pos, "<span class=\"");  // 각 항목 정보들은 <span class=" 로 구분된다!
+	}
+
+	if (p_movie_data->title_len == 0) {  // 이름 검색에 실패한 경우!!
+		// 영화 관람 기준 정보가 없어서 영화 검색에 실패한 경우에는 이름만 다시 추적해서 추가한다.
+		ap_string = strstr(ap_string, "<dt class=\"tit\">");
+		if (ap_string != NULL) {
+			CopyStringFromWebData(p_movie_data->title, &p_movie_data->title_len, ap_string + 16);  // +16은 <dt class="tit"> 길이 만큼 지나감
+		}
+	}
+	return p_pos;  // 작업이 끝난 위치를 반환한다.
+}
+
+void ConstructMovieList()  // HTML 정보를 사용해서 영화 목록 구성하기!
+{
+	DestoryMovieData();  // 리스트 박스에 등록되어 있던 기존 영화 정보를 제거한다!
+
+	void *p_edit = FindControl(1000);  // 에디트 컨트롤의 주소를 얻는다.
+	int len = Edit_GetLength(p_edit) + 1;  // 에디트 컨트롤에 저장된 문자열의 길이를 얻는다.
+	char *p_html_str = (char *)malloc(len); // 에디트 컨트롤에 저장된 문자열의 길이만큼 메모리 할당!
+	if (p_html_str != NULL) {
+		GetCtrlName(p_edit, p_html_str, len);  // 에디트 컨트롤에 저장된 문자열을 p_html_str에 복사한다.
+
+		char *p_pos = strstr(p_html_str, "<li><div class=\"thumb\">");  // 영화 목록의 시작 위치를 찾는다!
+		char *p_limit_pos = strstr(p_html_str, "<!-- 검색어 순위 -->");  // 영화 목록의 끝 위치를 찾는다!
+
+		while (p_pos != NULL && p_pos < p_limit_pos) {
+			p_pos = AddMovieInfo(p_pos + 23);  // + 23은 <li><div class="thumb"> 길이 만큼 지나감!
+			if (p_pos == NULL) break;  // 정보가 중단에 끝남!
+			p_pos = strstr(p_pos, "<li><div class=\"thumb\">");  // 영화 한 편의 시작 위치를 찾는다!
+		}
+
+		free(p_html_str);  // 에디트 컨트롤에 저장된 문자열을 복사하기 위해 할당했던 메모리 해제!
+	}
+}
+
+// 컨트롤을 조작했을 때 호출할 함수 만들기
+// 컨트롤의 아이디(a_ctrl_id), 컨트롤의 조작 상태(a_notify_code), 선택한 컨트롤 객체(ap_ctrl)
+void OnCommand(INT32 a_ctrl_id, INT32 a_notify_code, void *ap_ctrl)
+{
+	if (a_ctrl_id == 1010) LoadDataFromWebPage();  // "영화 페이지 소스 가져오기" 버튼이 눌러진 경우!
+	else if (a_ctrl_id == 1011) ConstructMovieList();  // "상영 예정작 보기" 버튼이 눌러진 경우!
+}
+
+// 등록된 사용자 목록에 추가된 항목을 출력하는 함수. 이 함수는 ListBox에 추가된 항목 수만큼 호출된다.
+void DrawMovieDataItem(int index, char *ap_str, int a_str_len, void *ap_data, int a_is_selected, RECT *ap_rect)
+{
+	// 리스트 박스의 각 항목이 선택되었을 때와 아닐 때의 항목 테두리 색상을 다르게 처리함!
+	if (a_is_selected) SelectPenObject(RGB(200, 255, 255));  // 선택 됨
+	else SelectPenObject(RGB(62, 77, 104));  // 선택 안됨
+
+	SelectBrushObject(RGB(62, 77, 104));  // 각 항목의 배경을 색상을 지정한다.
+	// 각 항목의 배경으로 사용할 사각형을 그린다.
+	Rectangle(ap_rect->left, ap_rect->top, ap_rect->right, ap_rect->bottom);
+
+	MovieData *p_data = (MovieData *)ap_data; // 현재 항목에 연결된 사용자 정보를 사용하기 위해 형변환을 한다.
+	SelectFontObject("굴림체", 12);  // 글꼴을 '굴림', 12 크기로 설정한다.
+
+	TextOut(ap_rect->left + 5, ap_rect->top + 5, RGB(100, 100, 200), "%s", p_data->ico_rate);  // 관람 기준 등급
+	TextOut(ap_rect->left + 105, ap_rect->top + 5, RGB(0, 200, 255), "%s", p_data->title); // 영화 제목
+	TextOut(ap_rect->left + 420, ap_rect->top + 5, RGB(200, 200, 210), "%s", p_data->etc); // 상영 시간과 개봉 일자
+	TextOut(ap_rect->left + 5, ap_rect->top + 21, RGB(255, 255, 255), "%s/%s", p_data->want, p_data->dont_want); // 좋아요, 글세요
+	TextOut(ap_rect->left + 105, ap_rect->top + 21, RGB(0, 232, 200), "%s", p_data->genre);  // 영화 장르
+	TextOut(ap_rect->left + 115 + p_data->genre_len * 6, ap_rect->top + 21, RGB(242, 200, 228), "감독 : %s", p_data->director);  // 감독
+	TextOut(ap_rect->left + 5, ap_rect->top + 37, RGB(228, 228, 100), "연기자 : %s", p_data->actor); // 출연진
+}
+
+// 컨트롤이 조작되면 OnCommand 함수를 호출하고 프로그램이 종료될 때 DestoryUserData 함수를 호출하도록 설정한다.
+CMD_USER_MESSAGE(OnCommand, DestoryMovieData, NULL)
+
+int main()
+{
+	ChangeWorkSize(600, 500); // 작업 영역을 설정한다.
+	Clear(0, RGB(82, 97, 124)); // 작업 영역을 RGB(82, 97, 124) 색으로 채운다!
+
+	// 웹 페이지 소스를 보여줄 에디트 컨트롤을 생성한다.
+	// (여러 줄 사용 모드, 가로/세로 자동 스크롤, 수직 스크롤바 사용, 엔터키 사용 모드)
+	CreateEdit(5, 5, 590, 200, 1000, ES_AUTOHSCROLL | ES_AUTOVSCROLL | ES_MULTILINE | ES_WANTRETURN | WS_VSCROLL | WS_HSCROLL);
+	void *p_movie_list = CreateListBox(5, 210, 590, 253, 1001, DrawMovieDataItem); // 리스트 박스를 생성
+	ListBox_SetItemHeight(p_movie_list, 53);  // 항목의 높이를 53으로 설정한다.
+
+	CreateButton("영화 페이지 소스 가져오기", 5, 468, 200, 28, 1010);  // 버튼 생성!
+	CreateButton("상영 예정작 보기", 210, 468, 200, 28, 1011);  // 버튼 생성!
+
+	ShowDisplay(); // 정보를 윈도우에 출력한다.
+	return 0;
+}
